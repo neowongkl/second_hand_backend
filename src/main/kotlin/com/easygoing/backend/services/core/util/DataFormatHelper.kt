0e2502fbc37fe8914d28c23256bf0722995a7e2f
Neo Wong <>
2021-10-18 01:18:30 +0800

    rename to util

---
+++ src/main/kotlin/com/easygoing/backend/services/core/util/DataFormatHelper.kt
@@ -0,0 +1,32 @@
+package com.easygoing.backend.services.core.util
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+@Component
+class DataFormatHelper {
+    @Autowired
+    private lateinit var objectMapper : ObjectMapper
+
+    //TODO("implement logger for exception handling")
+
+    fun <T> jsonToObject(json: String, clazz: Class<T>): T?{
+        return runCatching {
+            objectMapper.readValue(json, clazz)
+        }.onFailure { _exception->
+            println(_exception)
+            _exception.printStackTrace()
+        }.getOrNull()
+    }
+
+    fun objectToJsonString(targetObject: Any): String?{
+        return runCatching {
+            objectMapper.writeValueAsString(targetObject)
+        }.onFailure { _exception->
+            println(_exception)
+            _exception.printStackTrace()
+        }.getOrNull()
+    }
+
+}
