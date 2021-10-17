02c9f0c89e655589db96f3273c5e613d3677cb18
Neo Wong <>
2021-10-18 01:21:57 +0800

    clean code

--- src/test/kotlin/com/easygoing/backend/services/sample/controller/SampleMariaDbControllerTest.kt
+++ src/test/kotlin/com/easygoing/backend/services/sample/controller/SampleMariaDbControllerTest.kt
@@ -1,10 +1,8 @@
 package com.easygoing.backend.services.sample.controller
 
-import com.easygoing.backend.services.core.util.DataFormatHelper
 import com.easygoing.backend.services.sample.dao.SampleMariaDb
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Bean
 import org.springframework.http.HttpStatus
@@ -17,9 +15,6 @@ class SampleMariaDbControllerTest {
     fun restTemplate(): RestTemplate? {
         return RestTemplate()
     }
-
-    @Autowired
-    private lateinit var dataFormatHelper: DataFormatHelper
 
     val url = "http://localhost:8080/api/samplemaria"
 
