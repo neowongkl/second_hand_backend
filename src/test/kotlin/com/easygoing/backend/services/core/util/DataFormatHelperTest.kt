6f6c8287a8370987fa3da5fffb069deacbc85081
Neo Wong <>
2021-10-18 11:12:31 +0800

    clean code

--- src/test/kotlin/com/easygoing/backend/services/core/util/DataFormatHelperTest.kt
+++ src/test/kotlin/com/easygoing/backend/services/core/util/DataFormatHelperTest.kt
@@ -4,10 +4,8 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
-@ActiveProfiles(profiles = ["LOCAL"])
 class DataFormatHelperTest{
     @Autowired
     private lateinit var dataFormatHelper: DataFormatHelper
