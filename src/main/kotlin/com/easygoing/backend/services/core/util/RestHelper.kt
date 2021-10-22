5102899fb78c7b8e59dc47791f63d2af73cfe7fa
Neo Wong <>
2021-10-22 21:39:24 +0800

    Revert "implement sync rest helper by webclient"
    This reverts commit c24b15bca99e01e2adf08b9638302aca95a3e841.

--- src/main/kotlin/com/easygoing/backend/services/core/util/RestHelper.kt
+++
@@ -1,8 +0,0 @@
-package com.easygoing.backend.services.core.util
-
-interface RestHelper {
-    fun <T> getForEntity(url: String, responseClass: Class<T>): T?
-    fun <T> postForEntity(url: String, bodyObject: Any, responseClass: Class<T>): T?
-    fun <T> delete(url: String, responseClass: Class<T>): T?
-    fun <T> patchForEntity(url: String, bodyObject: Any, responseClass: Class<T>) : T?
-}
