c62d45994d7679f2d187701939616cf8b2a63b2c
Neo Wong <>
2021-10-22 17:06:24 +0800

    change resttemplate to webclient

--- src/test/kotlin/com/easygoing/backend/services/sample/controller/SampleMariaDbControllerTest.kt
+++ src/test/kotlin/com/easygoing/backend/services/sample/controller/SampleMariaDbControllerTest.kt
@@ -4,32 +4,41 @@ import com.easygoing.backend.services.sample.dao.SampleMariaDb
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.context.annotation.Bean
-import org.springframework.http.HttpStatus
-import org.springframework.web.client.RestTemplate
+import org.springframework.web.reactive.function.client.WebClient
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class SampleMariaDbControllerTest {
 
-    @Bean
-    fun restTemplate(): RestTemplate? {
-        return RestTemplate()
-    }
+    private val webClient = WebClient.create()
 
     val url = "http://localhost:8080/api/samplemaria"
 
     @Test
     fun listAll() {
-        val responseEntity = restTemplate()!!.getForEntity(url, Array<SampleMariaDb>::class.java)
-        assertEquals(HttpStatus.OK, responseEntity.statusCode)
-        assertNotNull(responseEntity.body)
+        val entity = webClient.get().uri(url)
+            .exchangeToMono { response->
+                if ( response.statusCode().is2xxSuccessful ){
+                    return@exchangeToMono response.bodyToMono(Array<SampleMariaDb>::class.java)
+                }else{
+                    return@exchangeToMono null
+                }
+            }
+            .block()
+        assertNotNull(entity)
     }
 
     @Test
     fun getById() {
-        val responseEntity = restTemplate()!!.getForEntity("$url/1", SampleMariaDb::class.java)
-        assertEquals(HttpStatus.OK, responseEntity.statusCode)
-        assertNotNull(responseEntity.body)
+        val entity = webClient.get().uri("$url/1")
+            .exchangeToMono { response->
+                if ( response.statusCode().is2xxSuccessful ){
+                    return@exchangeToMono response.bodyToMono(SampleMariaDb::class.java)
+                }else{
+                    return@exchangeToMono null
+                }
+            }
+            .block()
+        assertNotNull(entity)
     }
 
 }
--- src/test/kotlin/com/easygoing/backend/services/sample/controller/SampleMySqlDbControllerTest.kt
+++ src/test/kotlin/com/easygoing/backend/services/sample/controller/SampleMySqlDbControllerTest.kt
@@ -1,28 +1,29 @@
 package com.easygoing.backend.services.sample.controller
 
-import com.easygoing.backend.services.sample.dao.SampleMariaDb
 import com.easygoing.backend.services.sample.dao.SampleMySqlDb
-import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.context.annotation.Bean
-import org.springframework.http.HttpStatus
-import org.springframework.web.client.RestTemplate
+import org.springframework.web.reactive.function.client.WebClient
+
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class SampleMySqlDbControllerTest {
 
-    @Bean
-    fun restTemplate(): RestTemplate? {
-        return RestTemplate()
-    }
+    private val webClient = WebClient.create()
 
     val url = "http://localhost:8080/api/samplemysql"
 
     @Test
     fun listAll() {
-        val responseEntity = restTemplate()!!.getForEntity(url, Array<SampleMySqlDb>::class.java)
-        Assertions.assertEquals(HttpStatus.OK, responseEntity.statusCode)
-        Assertions.assertNotNull(responseEntity.body)
+        val entity = webClient.get().uri(url)
+            .exchangeToMono { response->
+                if ( response.statusCode().is2xxSuccessful ){
+                    return@exchangeToMono response.bodyToMono(Array<SampleMySqlDb>::class.java)
+                }else{
+                    return@exchangeToMono null
+                }
+            }.block()
+        assertNotNull(entity)
     }
 }
