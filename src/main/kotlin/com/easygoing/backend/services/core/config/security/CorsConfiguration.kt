edd42c713f7fc21acab3850f78b7d1eac570a783
Neo Wong <>
2021-11-06 13:40:12 +0800

    change cors setting to use security util

--- src/main/kotlin/com/easygoing/backend/services/core/config/security/CorsConfiguration.kt
+++ src/main/kotlin/com/easygoing/backend/services/core/config/security/CorsConfiguration.kt
@@ -1,12 +1,11 @@
 package com.easygoing.backend.services.core.config.security
 
-import com.easygoing.backend.services.core.config.security.dto.WebSecurity
-import com.easygoing.backend.services.core.util.DataFormatHelper
+import com.easygoing.backend.services.core.config.security.dto.CorsMapping
+import com.easygoing.backend.services.core.config.security.util.SecurityUtil
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.io.ClassPathResource
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import javax.annotation.PostConstruct
@@ -16,24 +15,20 @@ import javax.annotation.PostConstruct
 class CorsConfiguration {
 
     @Autowired
-    private lateinit var dataFormatHelper: DataFormatHelper
+    private lateinit var securityUtil: SecurityUtil
 
-    private var webSecurity: WebSecurity? = null
-
-    lateinit var mappingpath : String
+    private var corsMappings : List<CorsMapping>? = null
 
     @PostConstruct
     fun postConstruct(){
-        webSecurity = ClassPathResource(mappingpath).file.let {
-            dataFormatHelper.jsonFileToObject(it, WebSecurity::class.java)
-        }
+        corsMappings = securityUtil.corsMappings
     }
 
     @Bean
     fun corsMapping(): WebMvcConfigurer? {
         return object : WebMvcConfigurer {
             override fun addCorsMappings(registry: CorsRegistry) {
-                webSecurity?.corsMappings?.forEach { _corsMapping->
+                corsMappings?.forEach { _corsMapping->
                     registry.addMapping(_corsMapping.path)
                         .allowedOriginPatterns(*_corsMapping.allowedOrigins.toTypedArray())
                         .allowedMethods(*_corsMapping.allowedMethods.toTypedArray())
